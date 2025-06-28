fd25ffb2f6fd993db8b385bcdc454056c1e18a15
Mateus Antonio <mateusantonio23@live.com>
2025-06-28 16:15:53 -0300

    removidos arquivos gerados pelo vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,17 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+	<div class="">
+        <DataTable :value="customers" paginator :rows="5" :rowsPerPageOptions="[5, 10, 20, 50]" tableStyle="min-width: 50rem">
+            <Column field="name" header="Name" style="width: 25%"></Column>
+            <Column field="country.name" header="Country" style="width: 25%"></Column>
+            <Column field="company" header="Company" style="width: 25%"></Column>
+            <Column field="representative.name" header="Representative" style="width: 25%"></Column>
+        </DataTable>
+    </div>
 </template>
+
+<script setup>
+
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+
+</script>
